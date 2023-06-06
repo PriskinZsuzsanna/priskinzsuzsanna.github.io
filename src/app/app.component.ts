297a23da6d751943a82276193b9f6429b12c9b0b
Priskin Zsuzsanna <priskin.zsuzsanna@gmail.com>
2023-06-06 16:36:12 +0200

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faBars, faClose } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'priskin-site';
+
+  faBars = faBars
+  faClose = faClose
+
+  menuOpen: boolean = false
+
+  constructor(){
+
+  }
+
+  toggleMenu(){
+    this.menuOpen = !this.menuOpen
+  }
+
+
 }
