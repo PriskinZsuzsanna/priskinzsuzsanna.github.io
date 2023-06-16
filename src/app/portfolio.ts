8bb117cbf83623b7295511ad93135fdb08c48ad5
Zsuzsi <priskin.zsuzsanna@gmail.com>
2023-06-16 16:25:33 +0200

    ready1

--- src/app/portfolio.ts
+++ src/app/portfolio.ts
@@ -8,7 +8,7 @@ export const PORTFOLIO : Array<PortfolioItem> = [
         skills : ["React", "Hooks", "UseContext", "React Router", "Api", "Local Storage",
         "Styled Components", "Mobile First Workflow", "Keyframes"],
         url: "https://priskinzsuzsanna.github.io/ReactApps/",
-        image: "reactapps.png"
+        image: "react-apps.png"
     },
     {
         title : "Sunnyside Page",
@@ -19,6 +19,22 @@ export const PORTFOLIO : Array<PortfolioItem> = [
         image: "sunnyside.png"
     },
     {
+        title : "Angular Drag and Drop ToDo",
+        subtitle : "Angular Todo List",
+        category: "angular",
+        skills : ["Angular", "Typescript", "Add", "Delete", "Mark as done", "Filter", "CDK-DragDropModule", "Sass/Scss", "Light-Dark: change mode and detect mode", "local storage"],
+        url: "https://priskinzsuzsanna.github.io/angular-todo-frontendmentor/",
+        image: "angular-todo.png"
+    },
+    {
+        title : "Expenses Tracker",
+        subtitle : "Angular App",
+        category: "angular",
+        skills : ["Angular", "Typescript", "Class", "Routing", "Service", "Components", "FormsModule", "2 way data binding", "local storage"],
+        url: "https://priskinzsuzsanna.github.io/angular-expenses-tracker/",
+        image: "expenses.png"
+    },
+    {
         title : "js OOP dictionary",
         subtitle : "Angol egynyelvű szótár jegyzet funkcióval",
         category: "Javascript: Object Oriented Programing",
@@ -26,5 +42,37 @@ export const PORTFOLIO : Array<PortfolioItem> = [
         "Light-Dark Theme", "Playing Audio", "Show-Hide Elements"],
         url: "https://priskinzsuzsanna.github.io/FrontendMentor-SCSS-Sunnyside-Landing-Page/",
         image: "dictionary.png"
+    },
+    {
+        title : "Blog",
+        subtitle : "Angular Blog",
+        category: "Angular",
+        skills : ["Angular", "Router", "Components", "Service.ts", "SASS/SCSS", "Font-awesome", "Upload picture as file", "mobile-first"],
+        url: "https://priskinzsuzsanna.github.io/angular-blog/",
+        image: "blog.png"
+    },
+    {
+        title : "Musican Homepage",
+        subtitle : "Classical Guitarist Landing Page",
+        category: "CSS",
+        skills : ["Html", "Bootstrap", "Css", "Javascript", "Keyframes", "Animation"],
+        url: "https://priskinzsuzsanna.github.io/andrascsaki-homepage/",
+        image: "acsaki.png"
+    },
+    {
+        title : "Rock Paper Scissors",
+        subtitle : "Angular game",
+        category: "Angular",
+        skills : ["Angular", "Typescript", "using properties and interpolation","*ngIf", "keeping scores", "showing winner", "replay", "mobile-first"],
+        url: "https://priskinzsuzsanna.github.io/angular-rock-paper-scissors/",
+        image: "angular-rock-paper.png"
+    },
+    {
+        title : "Balett Műhely",
+        subtitle : "Landing Page",
+        category: "CSS",
+        skills : ["Html", "Bootstrap", "Css", "Javascript", "Keyframes", "Animation"],
+        url: "https://priskinzsuzsanna.github.io/Bootstrap-Balett-LandingPage/",
+        image: "balett.png"
     },
 ]
